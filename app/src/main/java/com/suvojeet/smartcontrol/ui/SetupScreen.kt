2b4a680042b7d31b477dde72a88f3815b115353b
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-30 00:09:06 +0530

    Fix build error: Opt-in to ExperimentalMaterial3Api in SetupScreen

--- app/src/main/java/com/suvojeet/smartcontrol/ui/SetupScreen.kt
+++ app/src/main/java/com/suvojeet/smartcontrol/ui/SetupScreen.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@OptIn(ExperimentalAnimationApi::class)
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun SetupScreen(
     onNavigateBack: () -> Unit,
