184844fb418f594a6f660c96de7024b04bd888c0
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-11-29 17:23:36 +0530

    Implement bulb status fetch on app start

--- app/src/main/java/com/suvojeet/smartcontrol/network/WizUdpController.kt
+++ app/src/main/java/com/suvojeet/smartcontrol/network/WizUdpController.kt
@@ -1,6 +1,8 @@
 package com.suvojeet.smartcontrol.network
 
 import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.DatagramPacket
@@ -33,6 +35,41 @@ object WizUdpController {
                 Log.d("WizController", "Sent to $ip: $json")
             } catch (e: Exception) {
                 Log.e("WizController", "Error sending command to $ip: ${e.message}")
+            }
+        }
+    }
+
+    suspend fun getBulbStatus(ip: String): Map<String, Any>? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val socket = DatagramSocket()
+                socket.soTimeout = 2000 // 2 second timeout
+                val address = InetAddress.getByName(ip)
+                val json = """{"method":"getPilot","params":{}}"""
+                val buffer = json.toByteArray()
+                val packet = DatagramPacket(buffer, buffer.size, address, WIZ_PORT)
+                socket.send(packet)
+
+                val receiveBuffer = ByteArray(1024)
+                val receivePacket = DatagramPacket(receiveBuffer, receiveBuffer.size)
+                socket.receive(receivePacket)
+                socket.close()
+
+                val response = String(receivePacket.data, 0, receivePacket.length)
+                Log.d("WizController", "Received from $ip: $response")
+                
+                val gson = Gson()
+                val type = object : TypeToken<Map<String, Any>>() {}.type
+                val map: Map<String, Any> = gson.fromJson(response, type)
+                
+                if (map.containsKey("result")) {
+                    map["result"] as? Map<String, Any>
+                } else {
+                    null
+                }
+            } catch (e: Exception) {
+                Log.e("WizController", "Error getting status from $ip: ${e.message}")
+                null
             }
         }
     }
